da8adbc98e28529b6e29e7694d8b3a11a6145a67
Arno _tt_aulaPHP <arno@advg.com.br>
2014-02-18 11:57:21 -0300

    EXERCICIO01.. script

---
+++ www/aula_ooPHP/exercicio1.php
@@ -0,0 +1,18 @@
+<?php
+// EXERCICIO 
+	require ('../../classes_arno/ContaCorrente.php');
+
+	$contaArno = new ContaCorrente();
+	$contaArno->agencia = 0432;
+	$contaArno->numero = 4321;
+	$contaArno->digito = 1;
+	$contaArno->tipo = 013;
+	$contaArno->saldo = 6000.00;
+	
+	var_dump($contaArno);
+	
+	
+	
+	
+
+?>
