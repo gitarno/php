84ddc0b291bfe0346a02cef9a218a27312ba8a58
Arno _tt_aulaPHP <arno@advg.com.br>
2014-02-18 10:21:30 -0300

    add Arquivo de Aula

--- www/aula_ooPHP/aula01.php
+++ www/aula_ooPHP/aula01.php
@@ -1,5 +1,21 @@
 <?php
 // arquivo na pasta WWW.. de um server.. ou acessivel via Browser...
 	require '../../classes_arno/Produto.php';
+	// chamando class
+	$produto = new Produto();
+	//atribuindo
+	$produto->codigo = 111;
+	$produto->produto = 'iPhone 5s';		
+	$produto->preco = 2900;
+	
+	$p2 = new Produto();
+	$p2->codigo = 222;
+	$p2->produto = 'Smatphone Samsung GAlaxy S4';
+	$p2->preco = 1000;
+	
+	
+	var_dump($produto);
+		
+		
 		
 ?>
