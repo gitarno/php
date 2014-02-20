b4534cf716f50f5a317e12e2dd26d58c25e3f209
Arno _tt_aulaPHP <arno@advg.com.br>
2014-02-20 10:41:02 -0300

    construindo um condicional.. em IN_ARRAY
    verificando se os valores são F ou J

--- classes_arno/cc.php
+++ classes_arno/cc.php
@@ -14,12 +14,24 @@ class cc extends Conta{
 	
 	// comandos SETTERS ...	
 	public function setTipo($p){
-		$this->tipo = $p;	
+		// converte o valor inserido em maiusculo...
+		$p = strtoupper((string)$p);
+		// condicional para F e J
+		//(($p=="F")||($p=="J"))?$this->tipo = $p:$this->tipo = $p;	
+
+		// estou testando se não tem os dois valores..ou mais..
+		if(!in_array($p,array("F","J"))){
+			$this->tipo = $p;
+			
+		}
+		
+		
+
 	}	
 
 	// comandos GETTERS ...
-	public function getTipo($p){
-		$this->tipo = $p;	
+	public function getTipo(){
+		return (string)$this->tipo;	
 	}
 	
 	
