dcd88a883e2cfbc6356d043aece2f5baf753ec12
Arno _tt_aulaPHP <arno@advg.com.br>
2014-02-24 11:50:06 -0300

    Comandos USE

--- classes_arno/produto.class.php
+++ classes_arno/produto.class.php
@@ -1,6 +1,8 @@
 <?php
 // trabalhando com namespace
 // acabar com conflito
+namespace Targettrust;
+
 class produto{
 	public $codigo;
 	public function __construct($p){
---
+++ www/aula_ooPHP/aula04-namespace.php
@@ -0,0 +1,8 @@
+<?php
+
+require '../../classes_arno/produto.class.php';
+
+use Targettrust\produto;
+$objeto = new produto(123);
+
+?>
