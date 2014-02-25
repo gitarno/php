ac1c1b636624899d4abf1499d433800dbc296ecc
Arno _tt_aulaPHP <arno@advg.com.br>
2014-02-25 09:13:08 -0300

    getCOMPOSER....

--- classes_arno/produto.class.php
+++ classes_arno/produto.class.php
@@ -1,6 +1,5 @@
 <?php
 // trabalhando com namespace
-// acabar com conflito
 namespace Targettrust;
 
 class produto{
@@ -12,4 +11,15 @@ class produto{
 	}
 }
 
+/* 
+Autoload >>>>>>>>>>>>>>>>> COMPOSER
+	- PSR-0 : http://www.php-fig.org/psr/psr-0/
+	- PSR-4 : http://www.php-fig.org/psr/psr-4/
+	composers.... https://getcomposer.org/
+	http://pear.php.net/
+		- JSON
+		- - Apache/log4php
+		- Packagist
+		- Web
+*/
 ?>
